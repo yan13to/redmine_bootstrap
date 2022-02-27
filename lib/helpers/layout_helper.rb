59d09bd48d02779b79910f38154836be560e91d2
Yanto Daryanto <yan13to@gmail.com>
2022-02-27 22:41:18 +0700

    more refactor

--- lib/helpers/layout_helper.rb
+++ lib/helpers/layout_helper.rb
@@ -1,6 +1,13 @@
 module Helpers
   module LayoutHelper
-    def page_title(title, options = {}, &block)
+    def page_card(options={}, &block)
+      options[:html_class] = options[:class]
+      options[:content] = capture(&block) if block_given?
+
+      render 'page_card', options
+    end
+
+    def page_title(title, options={}, &block)
       options[:title] = title
       options[:actions] = capture(&block) if block_given?
 
