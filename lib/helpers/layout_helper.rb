a3f1ef52b590d4fe5031a54f43051e6700651d2c
Yanto Daryanto <yan13to@gmail.com>
2022-03-16 19:34:37 +0700

    more conversion

--- lib/helpers/layout_helper.rb
+++ lib/helpers/layout_helper.rb
@@ -7,6 +7,24 @@ module Helpers
       render 'page_card', options
     end
 
+    def page_dropdown(title = nil, options={}, &block)
+      options[:title] = title
+      options[:listing] = capture(&block) if block_given?
+
+      render 'page_dropdown', options
+    end
+
+    def page_table(options={}, &block)
+      options[:html_class] = options[:class]
+      options[:body] = capture(&block) if block_given?
+
+      render 'page_table', options
+    end
+
+    def page_tab(options={})
+      render 'page_tab', options
+    end
+
     def page_title(title, options={}, &block)
       options[:title] = title
       options[:actions] = capture(&block) if block_given?
