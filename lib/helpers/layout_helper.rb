5669ea16f2b48b0f384945f7161c4494dbad675c
Yanto Daryanto <yan13to@gmail.com>
2022-03-22 07:08:01 +0700

    re-factor builder and add simple accordion button

--- lib/helpers/layout_helper.rb
+++ lib/helpers/layout_helper.rb
@@ -3,33 +3,49 @@ module Helpers
     def page_card(options={}, &block)
       options[:html_class] = options[:class]
       options[:content] = capture(&block) if block_given?
+      builder = options[:builder] || 'builder/page_card'
 
-      render 'page_card', options
+      render builder, options
     end
 
-    def page_dropdown(title = nil, options={}, &block)
+    def page_dropdown(title=nil, options={}, &block)
       options[:title] = title
       options[:listing] = capture(&block) if block_given?
+      builder = options[:builder] || 'builder/page_dropdown'
 
-      render 'page_dropdown', options
+      render builder, options
+    end
+
+    def page_simple_accordion_button(title=nil, target=nil, options={}, &block)
+      options[:title] = title
+      options[:target] = target
+      options[:content] = capture(&block) if block_given?
+      options[:html_class] = options[:class]
+      builder = options[:builder] || 'builder/page_simple_accordion_button'
+
+      render builder, options
     end
 
     def page_table(options={}, &block)
       options[:html_class] = options[:class]
       options[:body] = capture(&block) if block_given?
+      builder = options[:builder] || 'builder/page_table'
 
-      render 'page_table', options
+      render builder, options
     end
 
     def page_tab(options={})
-      render 'page_tab', options
+      builder = options[:builder] || 'builder/page_tab'
+
+      render builder, options
     end
 
     def page_title(title, options={}, &block)
       options[:title] = title
       options[:actions] = capture(&block) if block_given?
+      builder = options[:builder] || 'builder/page_title'
 
-      render 'page_title', options
+      render builder, options
     end
   end
 end
